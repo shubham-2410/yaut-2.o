c331ea85a7423ddb1d842abbb97d97c8033140b5
Shubham Phalke <phalkeshubham2003@gmail.com>
2025-11-07 23:02:36 +0530

    Added local and deployed urls to cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -17,7 +17,9 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin:"http://localhost:5173",//"https://yaut-frontend-20.vercel.app", // your frontend origin
+    origin: [
+      "http://localhost:5173",
+      "https://yaut-frontend-20.vercel.app"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // 👈 allow cookies / authorization headers
